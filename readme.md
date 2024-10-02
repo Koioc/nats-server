299a31b375bc11911f264d69e314b099112f5f9a
Medeiros <fabio@fernandesmedeiros.com>
2024-10-02 15:50:04 -0300

    feature: Consumo da fila Escrita na fila Logica para dar o ACK ou não

--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
+Criar aplicação:
+mvn clean install -DskipTests
+
+Rodar o Java:
+java -jar target/nats-api-0.0.1-SNAPSHOT.jar
+
 Subindo o servidor:
-docker run --name nats --network nats --rm -p 4222:4222 -p 8222:8222 nats --http_port 8222 --cluster_name NATS --cluster nats://0.0.0.0:6222
+docker run --name nats --rm -p 4222:4222 -p 8222:8222 nats --jetstream --server_name nats-server --http_port 8222
+
+Se inscrever na fila, usar o nats
+./nats subscribe ">" -s 0.0.0.0:4222
--- src/main/java/com/poc/nats_api/NatsController.java
+++ src/main/java/com/poc/nats_api/NatsController.java
@@ -1,4 +1,4 @@
-// package com.poc.nats_api;
+package com.poc.nats_api;
 
 // import io.nats.client.Connection;
 // import io.nats.client.Nats;
@@ -13,112 +13,113 @@
 // import java.time.Duration;
 
 import io.nats.client.Connection;
-import io.nats.client.Dispatcher;
 import io.nats.client.JetStream;
-import io.nats.client.JetStreamOptions;
+import io.nats.client.JetStreamApiException;
+import io.nats.client.JetStreamManagement;
+import io.nats.client.JetStreamSubscription;
+import io.nats.client.Message;
 import io.nats.client.Nats;
+import io.nats.client.PullSubscribeOptions;
 import io.nats.client.api.ConsumerConfiguration;
-import io.nats.client.api.ConsumerInfo;
+import io.nats.client.api.PublishAck;
+import io.nats.client.api.StorageType;
 import io.nats.client.api.StreamConfiguration;
 import io.nats.client.api.StreamInfo;
-import io.nats.client.api.StreamState;
-import io.nats.client.impl.NatsJetStream;
-import io.nats.client.impl.NatsMessage;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NatsController {
 
     private final Connection natsConnection;
     private final JetStream jetStream;
-    private static final String STREAM_NAME = "Lume";
-    private static final String SUBJECT_NAME = "POC";
-    private static final String DURABLE_CONSUMER_NAME = "durable_consumer";
+    private final JetStreamManagement jsm;
+    private final JetStreamSubscription sub;
 
-    public NatsController() throws IOException, InterruptedException {
-        // Conecta ao servidor NATS usando a variável de ambiente NATS_URL
-        String natsUrl = "nats://0.0.0.0:4222";
-        this.natsConnection = Nats.connect(natsUrl);
-        JetStream jetStream = natsConnection.jetStream(JetStreamOptions.DEFAULT_JS_OPTIONS);
+    private static final String STREAM_NAME = "lume";
+    private static final String SUBJECT_NAME = "poc";
+    private static final int DURATION_SECONDS = 10;
+    private static final int MAX_MESSAGES = 10;
+    private static final String DURABLE_CONSUMER_NAME = "lume-consumer";
 
-        // Step 2: Create a stream with persistence enabled (optional if already exists)
+    public NatsController() throws IOException, InterruptedException, JetStreamApiException {
+        // Conectar ao servidor NATS
+        natsConnection = Nats.connect("nats://0.0.0.0:4222");
+
+        // Gerenciamento de JetStream
+        jsm = natsConnection.jetStreamManagement();
+
+        // Configuração do Stream
         StreamConfiguration streamConfig = StreamConfiguration.builder()
-                .name(STREAM_NAME)
-                .subjects(SUBJECT_NAME)
-                .storageType(io.nats.client.api.StorageType.File) // or Memory if persistence is not needed
-                .build();
+            .name(STREAM_NAME)
+            .subjects(SUBJECT_NAME)
+            .storageType(StorageType.Memory)
+            .build();
 
-        jetStream.addStream(streamConfig);
+        // Adicionar o Stream
+        StreamInfo streamInfo = jsm.addStream(streamConfig);
+        jetStream = natsConnection.jetStream();
+
+        // Fechar a conexão
+        // natsConnection.close();
+
+        ConsumerConfiguration consumerConfig = ConsumerConfiguration.builder()
+        .durable(DURABLE_CONSUMER_NAME)
+        .build();
+
+        PullSubscribeOptions options = PullSubscribeOptions.builder()
+            .configuration(consumerConfig)
+            .build();
+
+        sub = jetStream.subscribe(SUBJECT_NAME, options);
     }
 
     @GetMapping("/nats")
-    public String consumir(@RequestParam String topico) {
-        try {
-            // // // Subscription subscription = natsConnection.subscribe("teste");
-            // // Subscription subscription = natsConnection.subscribe("teste", "teste");
+    public String consumir() throws JetStreamApiException, IOException, InterruptedException {
+        while (true) {
+            List<Message> messages = sub.fetch(MAX_MESSAGES, Duration.ofSeconds(DURATION_SECONDS));
+            if (messages.isEmpty()) {
+                break;
+            }
 
-            // // Message msg = subscription.nextMessage(Duration.ofSeconds(120));
-            // // String response = new String(msg.getQueueName());
-            // // return "Mensagem recebida: " + response;
+            for (Message msg : messages) {
+                // Processar a mensagem
+                String messageData = new String(msg.getData());
+                System.out.println("Mensagem recebida: " + messageData);
+                System.out.println("Meta: " + msg.metaData());
 
-            // Subscription subscription = natsConnection.subscribe(topico);
-            // Message msg = subscription.nextMessage(Duration.ofSeconds(120));
-            // String response = new String(msg.getData());
-            // return "Mensagem recebida: " + response;
+                // Simular processamento da mensagem
+                boolean processingSuccessful = processMessage(messageData);
 
-            ConsumerConfiguration consumerConfig = ConsumerConfiguration.builder()
-                .durable(DURABLE_CONSUMER_NAME)
-                .ackWait(Duration.ofSeconds(30)) // Configure acknowledgment timeout
-                .build();
-                
-            Dispatcher dispatcher = natsConnection.createDispatcher(msg -> {
-                try {
-                    // Step 4: Process the message
-                    String messageData = new String(msg.getData());
-                    System.out.println("Received message: " + messageData);
-
-                    // Simulate message processing
-                    boolean processingSuccessful = processMessage(messageData);
-
-                    if (processingSuccessful) {
-                        // Step 5: Acknowledge message only if processing was successful
-                        msg.ack();
-                        System.out.println("Message processed successfully and acknowledged.");
-                    } else {
-                        // If processing fails, the message is not acknowledged, it will be re-delivered
-                        System.out.println("Message processing failed, will not acknowledge.");
-                    }
-                } catch (Exception e) {
-                    // Log and do not acknowledge, message will be retried
-                    e.printStackTrace();
+                if (processingSuccessful) {
+                    System.out.println("Menssagem processada com sucesso!");
+                    msg.ack();
+                } else {
+                    // Se o processamento falhar, a mensagem não será confirmada e será reentregue
+                    System.out.println("Não foi possível processar a mensagem!");
                 }
-            });
 
-            // Step 6: Subscribe using JetStream and consumer configuration
-            jetStream.subscribe(SUBJECT_NAME, dispatcher, false, consumerConfig);
-
-            // Keep the connection alive to receive messages
-            Thread.sleep(60_000);                       
-        } catch (Exception e) {
-            return "Erro ao consumir mensagem: " + e.getMessage();
+                System.out.println("--------------------------------");
+            }
         }
+
+        return "Mensagens consumidas!";
     }
 
     @PostMapping("/nats")
-    public String publicar(@RequestParam String topico, @RequestParam String msg) {
-        try {
-            natsConnection.publish(topico, msg.getBytes());
-            return "OK publicada no tópico: " + topico + " a msg: " + msg;
-        } catch (Exception e) {
-            return "Erro ao publicada mensagem: " + e.getMessage();
-        }
+    public String publicar(@RequestParam String msg) throws JetStreamApiException, IOException {
+        PublishAck ack = jetStream.publish(SUBJECT_NAME, msg.getBytes());
+        return "Mensagem publicada: " + ack.getSeqno();
     }
 
     private static boolean processMessage(String message) {
-        // Simulate message processing
-        // Return false if processing fails, true if successful
-        return !message.contains("fail");
+        return !message.contains("erro");
     }
 }
